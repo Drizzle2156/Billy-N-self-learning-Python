39fa2e7ff6bd78e36e90fdadbf148f2580ba721f
Billy Noy <147870952+Drizzle2156@users.noreply.github.com>
2023-12-28 11:26:01 -0600

    Update hello.py

--- Lesson 01 Getting Started/hello.py
+++ Lesson 01 Getting Started/hello.py
@@ -1,2 +1,38 @@
+CHAPTER 1: GETTING STARTED
+Installed Python interpreter (IDLE + Python Launcher) from www.python.org. The interpreter allows for the Python REPL or SHELL which reads a piece of code, evaluates it, and then prints the result to the console in a loop.
+Installed VS Code which is a source code editor. 
+Ran into issue, Not being able to launch Python Launcher like before : r/learnpython. Got the problem with the popup window answered. 
+Just set open .py files with Python launcher app
+Install the Python extension from Microsoft. Extensions allow us to add languages and core functionalities to VS Code
+Run code in the terminal using the python3 command, don't type in the $
+
+Created a Python file titled hello.py and ran it 3 ways
+Carrot button -> run python file
+The other 2 ways required adding the folder I made which creates a workspace. Before I was just running the hello file without the folder added. 
+Then it enabled me just to type in python3 hello.py in the terminal to run, or right-click and run the python file in the terminal 
+
+Get Started Tutorial for Python in Visual Studio Code was my resource 
+The Python REPL • Python Land Tutorial  
+The Python Standard REPL: Try Out Code and Ideas Quickly 
+ The Python Standard REPL: Try Out Code and Ideas Quickly REPL explained
+So basically python files are scripts and the REPL is an interactive shell to enter Python commands and see the direct results
+
+Z shell on mac
+Python3 to enter Python REPL
+To exit Python REPL >>> quit()
+To exit Z shell it is: exit
+
+To cancel in python  Ctrl + C
+To access emojis on a Mac, you can press Command + Control + Spacebar at the same time
+SHIFT + OPTION + DOWN ARROW in order to copy and paste the line of code down
+
+COMMAND + / to comment out multiple lines 
+
+To select all instances of a word in Visual Studio Code on a Mac, you can use the keyboard shortcut SHIFT + CMD + L.
+
+CTRL + D: Selects each occurrence of a word one at a time
+
+
+
 greeting = 'Hello World I am new!'
 print(greeting)
