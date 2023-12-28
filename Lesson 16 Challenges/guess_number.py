1c46bfecff986883b283c5426bf65c72a64a5bb3
Billy Noy <147870952+Drizzle2156@users.noreply.github.com>
2023-12-28 11:17:41 -0600

    Update guess_number.py

--- Lesson 16 Challenges/guess_number.py
+++ Lesson 16 Challenges/guess_number.py
@@ -1,7 +1,14 @@
+Challenge #1, Guess number game which is using the knowledge from rps game and applying it to a new problem
+# Create a new game using knowledge from rps
+# The following are Command line arguments in terminal
+# python3 guess_number.py -n "Charly", enter
+# outputs -> Charly, guess which number I'm thinking of... 1, 2, or 3.
+# user inputs 1 2 or 3 in terminal and enter
+# outputs the picture in google doc notes
+
 import sys  # for interacting with python interpreter and system environment. such as command line arguments, sys.exit() to exit the script, and sys.version to findout
 import random
 from enum import Enum
-
 
 class numbers(Enum):
     computer_choice1 = 1
